a6f4de817e043bf73cd01c506273886a3be8e937
bobirjon <210030@akfauniversity.org>
2022-11-15 21:43:57 +0500

    implementing queue with templates

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 using namespace std;
@@ -13,16 +12,18 @@ public:
         return count==maxQueueSize;
     };
     void initializeQueue(int maxQueueSize){
-            queueFront = 0;
-            queueRear = maxQueueSize - 1;
+            queueFront = 1;
+            queueRear = 0;
             count = 0;
+            this->maxQueueSize = maxQueueSize;
+           list = new Type[maxQueueSize];
         };
     Type front(){
         if (!isEmptyQueue())
         {
             return list[queueFront];
         }throw runtime_error("is empty");
-        
+
 
     };
     Type back(){
@@ -31,19 +32,20 @@ public:
             return list[queueRear];
         }
         throw runtime_error("is full");
-        
+
     };
 
     void addQueue(Type queueElement){
-        if(!isFullQueue()){
+
+
+        if (!isFullQueue()) {
+
             queueRear = (queueRear + 1) % maxQueueSize;
             count++;
-            list[queueRear]  = queueElement;
-        }else{
-            cout << "Cannot add to a full queue." << "\n";
-            throw runtime_error("queue is full");
-        }
-    };
+            list[queueRear] = queueElement;
+        } else throw runtime_error("queue is full");
+    }
+
     void deleteQueue(){
         if(!isFullQueue()){
             count--;
@@ -53,7 +55,7 @@ public:
         }
     };
 
-    QueueType(int queueSize = 100){
+    explicit QueueType(int queueSize = 100){
         if(queueSize<=0){
             cout << "Size can't be negative \n";
             throw (runtime_error("size is negative"));
@@ -61,23 +63,54 @@ public:
         list = new Type[queueSize];
         maxQueueSize = queueSize;
 
-        queueFront =0;
-        queueRear = maxQueueSize-1;
+        queueFront  = 1;
+        queueRear = 0;
+
         count=0;
 
     }; // Function with default parameters
+
+    QueueType( const QueueType<Type>& otherQueue) {
+        initializeQueue();
+        maxQueueSize = otherQueue.maxQueueSize;
+        if(!otherQueue.isEmptyQueue()){
+            int temp = otherQueue.queueFront;
+            while (temp != otherQueue.queueRear) {
+                addQueue(otherQueue.list[temp]);
+                temp = (temp + 1) % otherQueue.maxQueueSize;
+            }
+            addQueue(otherQueue.list[temp]);
+            temp = (temp + 1) % otherQueue.maxQueueSize;
+            addQueue(otherQueue.list[temp]);
+
+        }
+    }
+
+
+
+
+
+
     QueueType(QueueType<Type>& otherQueue){ // To create a Queue object that copies another queue
         initializeQueue(otherQueue.maxQueueSize);
-        maxQueueSize = otherQueue.maxQueueSize;
-        if(!isEmptyQueue()){
-           Type temp = otherQueue.queueFront;
-           while (temp != otherQueue.queueRear)
-           {
-            addQueue(otherQueue.list[temp]);
-            temp = (temp+1) % otherQueue.maxQueueSize;
+
+//        maxQueueSize = otherQueue.maxQueueSize;
+        if(!otherQueue.isEmptyQueue()){
+           int temp = otherQueue.queueFront;
+
+           while (temp != otherQueue.queueRear) {
+               addQueue(otherQueue.list[temp]);
+               temp = (temp + 1) % otherQueue.maxQueueSize;
+
            }
+            addQueue(otherQueue.list[temp]);
+            temp = (temp + 1) % otherQueue.maxQueueSize;
+//            addQueue(otherQueue.list[temp]);
+
+        }else{
+            throw  runtime_error("empty is in construstor");
         }
-        
+
     };
 
     ~QueueType(){
@@ -85,24 +118,108 @@ public:
     };
 
     void print(){
-        for (Type i = queueFront; i < maxQueueSize; i++) {
-            if(list[i]!=0)
-            cout << list[i] << " ";
+        Type temp = queueFront;
+        while (temp!=queueRear){
+            cout << list[temp] << ' ';
+            temp = (temp+1) % maxQueueSize;
         }
-        
+        cout << list[temp] << ' ';
+
     }
 private:
     int maxQueueSize;
-    int count;
-    int queueFront;
-    int queueRear;
+    int count{};
+    int queueFront{};
+    int queueRear{};
     Type *list;
 };
 
 
 
+
+
+
+
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node() {
+        this->next = NULL;
+    }
+
+    Node(int x) {
+        this->data =x;
+        this->next = NULL;
+    }
+
+    void print() {
+        cout << data << " ";
+    }
+};
+
+
+
+
+class MyQueueLinkedList {
+    Node * head;
+    Node * tail;
+public:
+
+    MyQueueLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void push(int x) {
+        Node * temp = new Node(x);
+        if (head== nullptr){
+            head = temp;
+            tail = temp;
+        }else {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    int pop() {
+        int data;
+        data = head->data;
+        if (head->next== nullptr){
+            head = nullptr;
+            tail = nullptr;
+            return data;
+        }
+        head = head->next;
+        return data;
+
+    }
+    int peek() {
+        return head->data;
+    }
+
+    bool empty() {
+        return head== nullptr;
+    }
+};
+
+
+
+
+
+
+
+
+
+
 int main() {
-    
+
+
+
+
+
     QueueType<int> q;
     q.initializeQueue(100);
     q.addQueue(2);
@@ -111,13 +228,22 @@ int main() {
 
     q.addQueue(3);
     q.addQueue(73);
+    q.addQueue(73);
     q.deleteQueue();
     q.print();
-     
+    cout << endl;
 
     QueueType<int>q2(q);
-
+//    cout << q2.isEmptyQueue();
+//
     q2.print();
+    q2.addQueue(2);
+    q2.addQueue(2);
+    q2.addQueue(8);
+    q2.addQueue(3);
+    cout << endl;
+    q2.print();
+//
 
 
 
